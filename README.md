32b9c5f5bb106e625e1d6370a2acec23797286c9
JS2L <84076925+JS2L@users.noreply.github.com>
2022-12-19 11:03:44 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-4tfpiu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-t9takj)
---
+++ index.js
@@ -0,0 +1,15 @@
+// 유어클래스에 있는 반복문으로 구구단을 출력하는 코드를 복사 붙여넣기 하고 결과를 확인하세요.
+let num = 3;
+if (num >= 2 && num <= 9) {
+  console.log(num * 1); // 3
+  console.log(num * 2); // 6
+  console.log(num * 3); // 9
+  console.log(num * 4); // 12
+  console.log(num * 5); // 15
+  console.log(num * 6); // 18
+  console.log(num * 7); // 21
+  console.log(num * 8); // 24
+  console.log(num * 9); // 27
+} else {
+  console.log('2와 9사이의 수를 입력하세요.');
+}
